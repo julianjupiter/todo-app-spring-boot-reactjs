57cd65cabd7b67b51025005c796c9a17fcce9d7d
Julian Jupiter <julianjupiter.io@gmail.com>
2021-05-03 06:27:27 +0800

    Fix import order

--- todo-ui/src/App.js
+++ todo-ui/src/App.js
@@ -1,13 +1,13 @@
 import { RecoilRoot } from "recoil";
-import TodoList from "./components/TodoList";
+import TodoListFilters from "./components/TodoListFilters";
 import TodoCreator from "./components/TodoCreator";
+import TodoList from "./components/TodoList";
 
 import "primereact/resources/themes/mdc-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css"
 import 'primeflex/primeflex.css';
 import "primeicons/primeicons.css";
 import "./App.css";
-import TodoListFilters from "./components/TodoListFilters";
 
 const App = () => (
   <RecoilRoot>
